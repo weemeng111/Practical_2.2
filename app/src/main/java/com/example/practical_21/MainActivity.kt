1061e3a4ab84a32a9634950428ca3416fc70ec56
weemeng111 <weemeng111@gmail.com>
2020-03-09 00:22:52 +0800

    commit

--- app/src/main/java/com/example/practical_21/MainActivity.kt
+++ app/src/main/java/com/example/practical_21/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.practical_21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    private fun clickHandlerFunction(viewThatIsClicked: View) {
+        myButton.setOnClickListener {
+            clickHanderFunction(it)
+        }
+
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
     }
 
 }
